cdd5dbd2390f9881f7859d87f9a9ee4b341e46c4
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2025-04-04 19:53:51 +0800

    fix: correct the place to import HTMX script

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,17 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
-  return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>portfolio</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
-  );
+	return (
+		<html>
+			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>portfolio</title>
+				<link rel="stylesheet" href="/styles.css" />
+				<script src="https://unpkg.com/htmx.org@1.9.2"></script>
+			</head>
+			<body>
+				<Component />
+			</body>
+		</html>
+	);
 }
