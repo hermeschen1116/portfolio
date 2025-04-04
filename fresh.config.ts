6cc87f2aeb64ce22dd791a510dc6960ed23195dc
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2025-04-04 19:50:15 +0800

    fix(htmx): introduce htmx in head

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import htmx from "./plugins/htmx.ts";
 
 export default defineConfig({
-	plugins: [htmx(), tailwind()],
+	plugins: [tailwind()],
 });
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,32 @@
+import { Head } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
-  return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
-  );
+	const count = useSignal(3);
+	return (
+		<>
+			<Head>
+				{/* 这里引入 htmx 脚本 */}
+				<script src="https://unpkg.com/htmx.org@1.9.2"></script>
+			</Head>
+			<div class="px-4 py-8 mx-auto bg-[#86efac]">
+				<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+					<img
+						class="my-6"
+						src="/logo.svg"
+						width="128"
+						height="128"
+						alt="the Fresh logo: a sliced lemon dripping with juice"
+					/>
+					<h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+					<p class="my-4">
+						Try updating this message in the
+						<code class="mx-2">./routes/index.tsx</code> file, and refresh.
+					</p>
+					<Counter count={count} />
+				</div>
+			</div>
+		</>
+	);
 }
