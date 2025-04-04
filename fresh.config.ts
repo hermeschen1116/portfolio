0549cd2f329f5bdbeed5654051f9d04bb848deb6
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2025-04-04 19:39:59 +0800

    feat: introduce HTMX as plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import htmx from "./plugins/htmx.ts";
 
 export default defineConfig({
-  plugins: [tailwind()],
+	plugins: [htmx(), tailwind()],
 });
